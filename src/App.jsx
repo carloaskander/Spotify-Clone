cf6cbb3b435fca0bb51abefd79521b361132b8e9
Carlo Askander <carloaskander@live.se>
2023-10-04 22:06:16 +0200

    Second commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import { getAccessToken } from './utils/getAccessToken';
+import { getAccessTokenFromStorage } from './utils/getAccessTokenFromStorage';
+import Login from './pages/Login';
+import Dashboard from './components/Dashboard/Dashboard';
 
-function App() {
+function App({ spotifyApi }) {
+	const [token, setToken] = useState(getAccessTokenFromStorage());
+
+	useEffect(() => {
+		// setting the accessToken to token from session storage or from the url
+		const accessToken = getAccessTokenFromStorage() || getAccessToken();
+
+		if (accessToken) {
+			setToken(accessToken);
+			sessionStorage.setItem('spotifyToken', accessToken);
+			window.location.hash = '';
+		}
+	}, []);
+
 	return (
-		<div className="App">
-			<h1>Techover Self Made - Spotify</h1>
-		</div>
+		<Box className="App">
+			{token ? (
+				<Dashboard spotifyApi={spotifyApi} />
+			) : (
+				<Routes>
+					<Route path="*" element={<Login />} />
+				</Routes>
+			)}
+		</Box>
 	);
 }
-
 export default App;
